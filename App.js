fd0d380d26c219b6121e942edf6f76422e622ed4
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-12-31 15:19:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import  Home  from './components/Home';
+import  ContactUs  from './components/ContactUs';
 
-const Stack = createNativeStackNavigator();
+
 export default function App() {
-
+    const Stack = createNativeStackNavigator();
   return (
 
     <SafeAreaProvider>
       <NavigationContainer>
+          <Stack.Navigator initialRouterName='contact'
+          screenOptions={{
+            headerStyle:{backgroundColor:'#e2bee2'}
+          }}>
 
-        
+  <Stack.Screen name='home' component={Home} options={{title:"welcome"}}/>
+  <Stack.Screen name='contact' component={ContactUs} options={{title:"ContactUs"}}/>
+          </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
 
