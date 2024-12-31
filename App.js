00417c38be827dda041cbc6b095a13eaca608105
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-12-31 14:32:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+
+  return (
+
+    <SafeAreaProvider>
+      <NavigationContainer>
+
+        
+      </NavigationContainer>
+    </SafeAreaProvider>
+
+  );
+}
